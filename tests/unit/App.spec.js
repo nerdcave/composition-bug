8f448a5cb12119a7d136036082fdc32744312f1f
Jay Elaraj <jay@nerdcave.com>
2022-05-26 12:45:01 -0400

    Simplifies test case

--- tests/unit/App.spec.js
+++ tests/unit/App.spec.js
@@ -1,4 +1,3 @@
-/* eslint-disable */
 import VueCompositionAPI from "@vue/composition-api";
 import { expect } from "chai";
 import { mount, createLocalVue } from "@vue/test-utils";
@@ -9,21 +8,17 @@ const localVue = createLocalVue();
 localVue.use(VueCompositionAPI);
 
 describe("bug", function () {
-  // same test twice
-
-  it("renders first", async () => {
+  it("renders", () => {
     // useGlobal(); // calling this anywhere before it's called in the component seems to fix the issue
     const wrapper = mount(App, { localVue });
-    const counterEl = wrapper.findComponent({ name: "Counter" });
-    expect(counterEl.vm.countPlus1).to.eq(counterEl.vm.count + 1);
-    wrapper.destroy(); // seems to kill the countPlus1 computed from updating and the watchers
-  });
+    const counter = wrapper.findComponent({ name: "Counter" });
+    expect(counter.vm.countPlus1).to.eq(counter.vm.count + 1);
 
-  // this test fails but should pass
-  it("renders second", async () => {
-    const wrapper = mount(App, { localVue });
-    const counterEl = wrapper.findComponent({ name: "Counter" });
-    expect(counterEl.vm.countPlus1).to.eq(counterEl.vm.count + 1);
-    wrapper.destroy();
+    wrapper.destroy(); // seems to kill the countPlus1 computed and the watchers
+
+    const newVal = 123;
+    const { count, countPlus1 } = useGlobal();
+    count.value = newVal;
+    expect(countPlus1.value).to.eq(newVal + 1);
   });
 });
